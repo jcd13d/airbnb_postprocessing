1da20615a451195ff81ddddc73337df466ff224d
Justin DiEmmanuele <justindiemmanuele@Justins-Air.attlocal.net>
2022-04-03 22:16:23 -0400

    .

---
+++ build/run.sh
@@ -0,0 +1,7 @@
+spark-submit --deploy-mode cluster \
+--master yarn \
+--num-executors 3 \
+--executor-cores 3 \
+--executor-memory 3g\
+–conf spark.yarn.submit.waitAppCompletion=false \
+aws_pyspark_test.py

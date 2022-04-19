644cb31fd033c52e3ff55f0436e97bba7b33a104
Justin DiEmmanuele <justindiemmanuele@Justins-Air.attlocal.net>
2022-04-19 19:21:47 -0400

    refactored out config lambda

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 client = boto3.client('emr')
 
@@ -6,48 +7,12 @@ print("Starting..\n")
 
 
 def handler(event, context):
-    response = client.run_job_flow(
-        Name='cli-emr-cluster-lambda',
-        LogUri='s3://airbnb-scraper-bucket-0-0-1/emr_logs/',
-        ReleaseLabel='emr-5.35.0',
-        Instances={
-            'MasterInstanceType': 'm5.xlarge',
-            'SlaveInstanceType': 'm5.xlarge',
-            'InstanceCount': 2,
-            'KeepJobFlowAliveWhenNoSteps': False,
-            'TerminationProtected': False,
-            'Ec2SubnetId': 'subnet-02e3bd361019638af'
-            # 'Ec2SubnetId': 'subnet-0e603a8b787e76dc8'
-            # 'Ec2SubnetId': 'subnet-0ebd9fe1b02a38e96'
-            # 'Ec2SubnetId': 'subnet-06b6292a876432928'
-            # 'Ec2SubnetId': 'subnet-0af6b8799829bcd29'
-            # 'Ec2SubnetId': 'subnet-0560d0f903be012ef'
-        },
-        Applications=[{'Name': 'Spark'}],
-        VisibleToAllUsers=True,
-        JobFlowRole='EMR_EC2_DefaultRole',
-        ServiceRole='EMR_DefaultRole',
-        Steps=[
-            {
-                'Name': 'postprocessing',
-                'ActionOnFailure': 'TERMINATE_CLUSTER',
-                'HadoopJarStep': {
-                    'Jar': 'command-runner.jar',
-                    'Args': ["spark-submit",
-                             "--py-files",
-                             "s3://airbnb-scraper-bucket-0-0-1/postprocessing_files/dependencies.zip",
-                             "s3://airbnb-scraper-bucket-0-0-1/postprocessing_files/postprocessing.py"]
-                }
-            }
-        ],
-        BootstrapActions=[
-            {
-                "Name": "EMR Setup",
-                "ScriptBootstrapAction": {
-                    "Path": "s3://airbnb-scraper-bucket-0-0-1/postprocessing_files/set_up_cluster.sh"
-                }
-            }
-        ]
-    )
+    s3 = boto3.resource('s3')
+
+    content_object = s3.Object('test', 'sample_json.txt')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    config = json.loads(file_content)
+
+    response = client.run_job_flow(**config)
 
     return response
